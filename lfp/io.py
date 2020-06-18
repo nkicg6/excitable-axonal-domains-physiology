c7e7230a5d0a50731bb6789d653693f4294f09f9
Nicholas George <nicholas.george32@gmail.com>
2020-06-18 16:54:48 -0600

    first commit

---
+++ lfp/io.py
@@ -0,0 +1,6 @@
+# io files for LFP analysis
+import os
+
+def io_return_data_endswith(path, endswith):
+    """returns sorted list of files from base `path` ending with `endswith`"""
+    return sorted([os.path.join(base,f) for f in os.listdir(base) if f.endswith(endswith)])
