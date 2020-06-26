b4849ddbf315c547f2958cbb67b73221c7e3a1fc
Nicholas George <nicholas.george32@gmail.com>
2020-06-25 21:33:03 -0600

    black formatting

--- lfp/parsing.py
+++ lfp/parsing.py
@@ -19,7 +19,8 @@ def _IO_parse_csv(path: str) -> dict:
         for rowdict in reader:
             outlist.append(rowdict)
 
-    return {item['Fname-ref'].strip(".abf"):item for item in outlist}
+    return {item["Fname-ref"].strip(".abf"): item for item in outlist}
+
 
 def _filter_protocol(indict: dict, use_ref_case: str) -> dict:
     prot_filtered = {}
@@ -28,18 +29,23 @@ def _filter_protocol(indict: dict, use_ref_case: str) -> dict:
             prot_filtered[k] = indict[k]
     return prot_filtered
 
+
 def _add_unique_id(indict: dict) -> dict:
     for item in indict.keys():
-        animal = indict[item]['Animal-ref']
-        slice_ = indict[item]['Slice-ref']
-        repref = indict[item]['Rep-ref']
+        animal = indict[item]["Animal-ref"]
+        slice_ = indict[item]["Slice-ref"]
+        repref = indict[item]["Rep-ref"]
         unique = "_".join([animal, slice_, repref])
-        indict[item]['uniqueid'] = unique
+        indict[item]["uniqueid"] = unique
     return indict
+
 
 # JSON parsing functions
 def _IO_get_json_data(base: str, endswith: str) -> list:
-    return sorted([os.path.join(base,f) for f in os.listdir(base) if f.endswith(endswith)])
+    return sorted(
+        [os.path.join(base, f) for f in os.listdir(base) if f.endswith(endswith)]
+    )
+
 
 def _IO_return_data(flist: list) -> list:
     good = []
@@ -49,14 +55,22 @@ def _IO_return_data(flist: list) -> list:
             good.append(d)
     return good
 
-def _jsonlist_to_dict(jsonlist: list) -> dict:
-    return {item['short_filename']:item for item in jsonlist}
 
-def IO_parse_json_main(path: str, endswith: str) ->dict:
+def _jsonlist_to_dict(jsonlist: list) -> dict:
+    return {item["short_filename"]: item for item in jsonlist}
+
+def get_csv(path: str):
+    for p in os.listdir(path):
+        if p.endswith(".csv"):
+            return os.path.join(path, p)
+    return None
+
+def IO_parse_json_main(path: str, endswith: str) -> dict:
     jfiles = _IO_get_json_data(path, endswith)
     jlist = _IO_return_data(jfiles)
     todict = _jsonlist_to_dict(jlist)
     return todict
+
 
 def merge_csv_json(csv_dict: dict, json_data: dict) -> dict:
     new_d = {}
--- setup.py
+++ setup.py
@@ -1,1 +1,9 @@
 from setuptools import setup
+
+setup(
+    name="thesis_ephys",
+    version="0.1-dev",
+    description="Analysis routines for patch clamp and extracellular electrophysiology.",
+    packages=["lfp", "patch"],
+    entry_points={"console_scripts": ["lfp = lfp.__main__:main"]},
+)
