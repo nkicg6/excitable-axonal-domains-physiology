769391a2addb47df90f60aff935b5b9ff329466d
Nicholas George <nicholas.george32@gmail.com>
2020-06-26 09:44:44 -0600

    args required. add validate

--- lfp/args.py
+++ lfp/args.py
@@ -1,5 +1,6 @@
 # command line options and parsing
 import argparse
+import os
 
 parser = argparse.ArgumentParser(description="Extracellular recording data analysis")
 
@@ -7,11 +8,17 @@ parser.add_argument(
     "-d",
     "--data-directory",
     dest="data_directory",
+    required = True,
     help="Directory with data and metadata files",
 )
 parser.add_argument(
     "-a",
     "--analysis-type",
     dest="analysis_type",
+    required=True,
     help="type of analyis to run. Currently supported options include: 'io' ",
 )
+
+def validate_args(parser):
+
+    pass
