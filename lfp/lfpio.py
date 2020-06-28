c5f646ebcfd8e3006841a939abfee65ab29b1ada
Nicholas George <nicholas.george32@gmail.com>
2020-06-28 17:41:53 -0600

    handle multiple peaks by selecting first

--- lfp/lfpio.py
+++ lfp/lfpio.py
@@ -38,10 +38,10 @@ def _find_peak(x_subset, y_subset, peak_direction):
         peak_y = np.array([])
         peak_x = np.array([])
         return {
-        "peak_subset_index": peak_ind,
-        "peak_x_val": peak_x,
-        "peak_y_val": peak_y,
-        "peak_dict": peak_dict,
+            "peak_subset_index": peak_ind,
+            "peak_x_val": peak_x,
+            "peak_y_val": peak_y,
+            "peak_dict": peak_dict,
         }
     if len(peak_ind) > 1:
         print(f"[PEAK INDICIES N = {len(peak_ind)}]")
@@ -51,19 +51,19 @@ def _find_peak(x_subset, y_subset, peak_direction):
         peak_y = np.asarray([y_subset[peak_ind]])
         peak_x = np.asarray([x_subset[peak_ind]])
         return {
-        "peak_subset_index": peak_ind,
-        "peak_x_val": peak_x,
-        "peak_y_val": peak_y,
-        "peak_dict": peak_dict,
+            "peak_subset_index": peak_ind,
+            "peak_x_val": peak_x,
+            "peak_y_val": peak_y,
+            "peak_dict": peak_dict,
         }
     if len(peak_ind) == 1:
         peak_y = y_subset[peak_ind]
         peak_x = x_subset[peak_ind]
         return {
-        "peak_subset_index": peak_ind,
-        "peak_x_val": peak_x,
-        "peak_y_val": peak_y,
-        "peak_dict": peak_dict,
+            "peak_subset_index": peak_ind,
+            "peak_x_val": peak_x,
+            "peak_y_val": peak_y,
+            "peak_dict": peak_dict,
         }
 
 
@@ -74,6 +74,7 @@ def _get_amplitude(peak_y_val, baseline_y):
         return abs(baseline_y - peak_y_val[0])
     if peak_y_val.size < 1:
         return 0
+
 
 def _fmt_results_dict(amplitude, subset_x, subset_y, peak_results, entry):
     return {
