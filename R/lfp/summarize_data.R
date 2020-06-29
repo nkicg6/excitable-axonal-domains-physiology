00b74286fc8e82be87d55ad5baa08312810ea7a5
Nicholas George <nicholas.george32@gmail.com>
2020-06-29 11:41:39 -0600

    start analysis in R

--- R/lfp/summarize_data.R
+++ R/lfp/summarize_data.R
@@ -10,3 +10,13 @@ data_dirs <- c("/Users/nick/Dropbox/lab_notebook/projects_and_data/mnc/analysis_
          "/Users/nick/Dropbox/lab_notebook/projects_and_data/mnc/analysis_and_data/extracellular_lfp/data/2020-03-13")
 
 
+of_files <- purrr::map(data_dirs, list.files(pattern="_io_results.json", full.names=T))
+allFiles <- list.files(data_dirs[1], pattern = "_io_results.json", full.names = T)
+
+io_file_from_json <- function(pathvector){
+  json_files = list.files(pathvector, pattern = "_io_results.json", full.names = T)
+  read_in = purrr::map(json_files, jsonlite::fromJSON)
+  return(bind_rows(read_in))
+}
+
+io_file_from_json(data_dirs[1])
