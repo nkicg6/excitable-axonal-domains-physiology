2ebd8707774ee4ff483633550702a226be8a7c0b
Nicholas George <nicholas.george32@gmail.com>
2020-07-16 18:33:27 -0600

    updates... trying to make it work

--- lfp/refractoriness-scratch.py
+++ lfp/refractoriness-scratch.py
@@ -118,8 +118,66 @@ plt.show()
 
 ## This method above seems to work well.
 ## we need to work with the first 16 sweeps (10ms IPI to 2ms IPI).
-## Need a function that takes the five files and returns a dict with:
+## Need a function that takes the five files and returns a dict with the following information per sweep:
 ## - mean y data from channel 0
 ## - dataPointsPerMs
-## - mean y data from channel 1
+## - stim_indexes
 ## should take in a list of files, return the data structure described above
+
+
+def make_data_structure(files: list, max_sweep: int) -> dict:
+    structure = {}
+    sample_freq = pyabf.ABF(files[0]).dataPointsPerMs
+    for sweep in range(max_sweep + 1):
+        x, mean_data = calc_refract_mean(files, sweep, channel=0)
+        _, mean_stim_data = calc_refract_mean(files, sweep, channel=1)
+        signal_indicies = _find_signal_index(mean_stim_data)
+        structure[f"sweep_{sweep}"] = {
+            "x": x,
+            "y": mean_data,
+            "stim_indicies": signal_indicies,
+            "ms_sample_rate": sample_freq,
+        }
+    return structure
+
+def apply_artifat_template(data_struct: dict, ms_offset: float =5):
+    ds = data_struct.copy()
+    offset = int(ds['sweep_0']['ms_sample_rate']*ms_offset) # assumes all offsets equal
+    stims = ds['sweep_0']['stim_indicies']
+    template = ds['sweep_0']['y'][stims[0]:stims[0]+offset]
+    l_sweep = len(ds['sweep_0']['y'])
+    for k in ds.keys():
+        stim_one, stim_two = ds[k]['stim_indicies']
+        y_template = np.zeros(l_sweep)
+        #y_template[stim_one : stim_one + offset] = template
+        y_template[stim_two : stim_two + offset] = template
+        subtracted = ds[k]['y'] - y_template
+        ds[k]['no_artifact'] = subtracted
+        ds[k]['ms_offset'] = ms_offset
+    return ds
+
+def _calc_mean_conditioning_amplitude(data_struct: dict, offset_ms=2):
+    ds = data_struct.copy()
+    stop_offset = int(ds['sweep_0']['stim_indicies'][0]+ds['sweep_0']['ms_sample_rate']*offset_ms)
+    pass
+
+
+experiment = make_data_structure(exp_data["199034-lear_2_1"], 16)
+new_exp = apply_artifat_template(experiment, 3)
+##
+
+fig = plt.figure(figsize=(8, 8))
+ax1 = fig.add_subplot(111)
+#for i in new_exp.keys():
+#    ax1.plot(new_exp[i]['x'], new_exp[i]["no_artifact"], label=f"no artifact {i}", alpha=0.4)
+ax1.plot(new_exp['sweep_0']['x'], new_exp['sweep_0']["no_artifact"], label="s0 no artifact", alpha=0.4)
+ax1.plot(new_exp['sweep_15']['x'], new_exp['sweep_15']["no_artifact"], label="s12 no artifact", alpha=0.4)
+#ax1.legend()
+plt.show()
+
+for k in exp_data.keys():
+    try:
+        print(f"key {k} is {exp_data[k][0]}")
+    except IndexError:
+        print(exp_data[k])
+######### This is not working. try just subtracting the
