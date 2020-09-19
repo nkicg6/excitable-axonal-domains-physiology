cce94558ac3ce7b0f4be1fe4afe8ed1bc352ddad
Nicholas George <nicholas.george32@gmail.com>
2020-09-19 16:12:19 -0600

    added bash script to update metadata. Just add file to list

---
+++ update_metadata_db.sh
@@ -0,0 +1,18 @@
+#! /bin/sh
+# update metadata
+paths=("/Users/nick/Dropbox/lab_notebook/projects_and_data/mnc/analysis_and_data/patch_clamp/data/passive_membrane_properties_2019-12-29"\
+           "/Users/nick/Dropbox/lab_notebook/projects_and_data/mnc/analysis_and_data/patch_clamp/data/passive_membrane_properties_2019-12-30"\
+           "/Users/nick/Dropbox/lab_notebook/projects_and_data/mnc/analysis_and_data/patch_clamp/data/passive_membrane_properties_2020-01-01"\
+           "/Users/nick/Dropbox/lab_notebook/projects_and_data/mnc/analysis_and_data/patch_clamp/data/passive_membrane_properties_2020-01-02"\
+           "/Users/nick/Dropbox/lab_notebook/projects_and_data/mnc/analysis_and_data/patch_clamp/data/passive_membrane_properties_2020-01-03"\
+           "/Users/nick/Dropbox/lab_notebook/projects_and_data/mnc/analysis_and_data/patch_clamp/data/passive_membrane_properties_2020-01-04"\
+           "/Users/nick/Dropbox/lab_notebook/projects_and_data/mnc/analysis_and_data/patch_clamp/data/passive_membrane_properties_2020-01-15"\
+           "/Users/nick/Dropbox/lab_notebook/projects_and_data/mnc/analysis_and_data/patch_clamp/data/passive_membrane_properties_2020-01-16")
+
+database="/Users/nick/Dropbox/lab_notebook/projects_and_data/mnc/analysis_and_data/patch_clamp/data/summary_data/spiking_201912-202001/patch_metadata.db"
+
+for val in ${paths[*]};
+do
+    python patch_clamp/archive.py -db $database -dir $val
+done                             
+echo "done!"
