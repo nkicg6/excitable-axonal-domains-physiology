9340a9168786a542e83584a9c429bddab1acbd81
Nicholas George <nicholas.george32@gmail.com>
2020-10-28 18:30:52 -0600

    add metadata to new database. changed path

--- shell/update_metadata_db.sh
+++ shell/update_metadata_db.sh
@@ -9,7 +9,7 @@ paths=("/Users/nick/Dropbox/lab_notebook/projects_and_data/mnc/analysis_and_data
            "/Users/nick/Dropbox/lab_notebook/projects_and_data/mnc/analysis_and_data/patch_clamp/data/passive_membrane_properties_2020-01-15"\
            "/Users/nick/Dropbox/lab_notebook/projects_and_data/mnc/analysis_and_data/patch_clamp/data/passive_membrane_properties_2020-01-16")
 
-database="/Users/nick/Dropbox/lab_notebook/projects_and_data/mnc/analysis_and_data/patch_clamp/data/summary_data/spiking_201912-202001/patch_data.db"
+database="/Users/nick/Dropbox/lab_notebook/projects_and_data/mnc/analysis_and_data/patch_clamp/data/summary_data/spiking_201912-202001/patch_data_batch.db"
 
 for val in ${paths[*]};
 do
