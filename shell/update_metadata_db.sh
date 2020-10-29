84d41905c67d19f38da0d16211f6b4392fb6ff40
Nicholas George <nicholas.george32@gmail.com>
2020-10-28 18:36:03 -0600

    fixed error added metadata table first

--- shell/update_metadata_db.sh
+++ shell/update_metadata_db.sh
@@ -1,6 +1,6 @@
 #! /bin/sh
 # update metadata
-paths=("/Users/nick/Dropbox/lab_notebook/projects_and_data/mnc/analysis_and_data/patch_clamp/data/passive_membrane_properties_2019-12-29"\
+PATHS=("/Users/nick/Dropbox/lab_notebook/projects_and_data/mnc/analysis_and_data/patch_clamp/data/passive_membrane_properties_2019-12-29"\
            "/Users/nick/Dropbox/lab_notebook/projects_and_data/mnc/analysis_and_data/patch_clamp/data/passive_membrane_properties_2019-12-30"\
            "/Users/nick/Dropbox/lab_notebook/projects_and_data/mnc/analysis_and_data/patch_clamp/data/passive_membrane_properties_2020-01-01"\
            "/Users/nick/Dropbox/lab_notebook/projects_and_data/mnc/analysis_and_data/patch_clamp/data/passive_membrane_properties_2020-01-02"\
@@ -9,10 +9,12 @@ paths=("/Users/nick/Dropbox/lab_notebook/projects_and_data/mnc/analysis_and_data
            "/Users/nick/Dropbox/lab_notebook/projects_and_data/mnc/analysis_and_data/patch_clamp/data/passive_membrane_properties_2020-01-15"\
            "/Users/nick/Dropbox/lab_notebook/projects_and_data/mnc/analysis_and_data/patch_clamp/data/passive_membrane_properties_2020-01-16")
 
-database="/Users/nick/Dropbox/lab_notebook/projects_and_data/mnc/analysis_and_data/patch_clamp/data/summary_data/spiking_201912-202001/patch_data_batch.db"
+DB_PATH="/Users/nick/Dropbox/lab_notebook/projects_and_data/mnc/analysis_and_data/patch_clamp/data/summary_data/spiking_201912-202001/patch_data_batch.db"
+DB_SCHEMA_PATH="/Users/nick/personal_projects/thesis/thesis_ephys/ephys/sql/schema.sqlite"
 
-for val in ${paths[*]};
+python scripts/mk_table.py -db $DB_PATH -schema $DB_SCHEMA_PATH
+for val in ${PATHS[*]};
 do
-    python patch_clamp/archive.py -db $database -dir $val
+    python patch_clamp/archive.py -db $DB_PATH -dir $val
 done                             
 echo "done!"
