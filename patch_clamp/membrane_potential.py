194d16ed6df3c1c99b3cc84c32a95821f14bc308
Nicholas George <nicholas.george32@gmail.com>
2020-11-10 10:10:32 -0700

    More edits for scripts for membrane potential table

--- patch_clamp/membrane_potential.py
+++ patch_clamp/membrane_potential.py
@@ -1,33 +1,23 @@
 # measure RMP and add to DB
 import numpy as np
-import patch_clamp.database as db
 import patch_clamp.utils as utils
 
 # Purpose:
 # measure the RMP for sweep 0 from each file for cc_01-steps protocol and add it to a
 # simple database table with columns fname, fpath, sweep, measured_rmp
-# RMP is the mean of the first 0.5s of the sweep.
 
-con = db.persistent_connection_to_db(db.DATABASE_PATH)
-con.execute(TABLE)
+
+def read_and_filter(fpath, half_ms_window, degree):
+    abf_file = utils.read_abf_IO(fpath, 0, 0)
+    filtered = utils.abf_golay(abf_file, half_ms_window, degree)
+    return filtered
 
 
 def get_mean_prestim(abfd, stop_index=10620):
-    """takes mean of `filtered` data between 0 and `stop_index`.
-    adds this as key `mean_rmp`"""
-    m = np.mean(abfd["filtered"][0:stop_index])
-    abfd["mean_rmp"] = float(m)
+    """takes mean and median of `filtered` data between 0 and `stop_index`.
+    adds this as key `mean_rmp` and `median_rmp`"""
+    mean_rmp = np.mean(abfd["filtered"][0:stop_index])
+    median_rmp = np.median(abfd["filtered"][0:stop_index])
+    abfd["mean_rmp"] = float(mean_rmp)
+    abfd["median_rmp"] = float(median_rmp)
     return abfd
-
-
-if __name__ == "__main__":
-    cc01paths = db.get_paths_for_protocol(db.DATABASE_PATH, "cc_01-steps")
-    half_ms_window = 11  # data points for filter
-    degree = 3  # based on Mae's paper
-    for stepfile in cc01paths:
-        abf_file = utils.abf_golay(
-            utils.read_abf_IO(stepfile, 0, 0), half_ms_window, degree
-        )
-        abf_file = get_mean_prestim(abf_file)
-        db.add_to_db_parameterized(db.DATABASE_PATH, QUERY, abf_file)
-    print("Done")
--- scripts/add_resting_potential.py
+++ scripts/add_resting_potential.py
@@ -1,19 +1,30 @@
 # add resting potential table
+# RMP is the mean of the first 0.5s of the sweep. Sweep ending index is hard coded into
+# `stop_index` parameter of membrane_potential.py
+
 import argparse
 import os
 import sys
 
 import patch_clamp.database as db
+import patch_clamp.membrane_potential as mp
 
 parser = argparse.ArgumentParser(
     description="Measure resting membrane potential and add to database"
 )
 parser.add_argument("-db", help="database path")
+parser.add_argument("-window", help="Half ms window for filter", type=int)
+parser.add_argument("-degree", help="Degree for filter", type=int)
+parser.add_argument("-query", help="generic string for DB insertion")
+
 
 if __name__ == "__main__":
     print("[add_resting_potential] Starting...")
     args = parser.parse_args()
     DB_PATH = args.db
+    DEGREE = args.degree
+    WINDOW = args.window
+    QUERY = args.query
 
     if not os.path.exists(DB_PATH):
         sys.exit(f"database {DB_PATH} doesn't exist. Exiting")
@@ -21,6 +32,8 @@ if __name__ == "__main__":
     print(f"Paths: {PATHS}")
     for path in PATHS:
         print(f">>>> Analyzing {path}\n\n")
-        # get vals, do work
+        current_d = mp.read_and_filter(path, WINDOW, DEGREE)
+        abf_file = mp.get_mean_prestim(current_d)  # use default stop index
         print(">>>> Adding to database")
+        db.add_to_db_parameterized(DB_PATH, QUERY, abf_file)
     print("Done")
