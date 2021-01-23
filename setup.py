59c50bbef1e63937397a0b904e20cd1c1d8363f8
Nicholas George <nicholas.george32@gmail.com>
2021-01-23 08:51:36 -0700

    updated setup file. bumped version.

--- setup.py
+++ setup.py
@@ -2,8 +2,15 @@ from setuptools import setup
 
 setup(
     name="thesis_ephys",
-    version="0.1-dev",
+    version="0.5-presubmission",
     description="Analysis routines for patch clamp and extracellular physiology.",
+    install_requires=[
+        "numpy==1.19.2",
+        "pyabf==2.2.8",
+        "pytest==6.0.2",
+        "matplotlib==3.3.2",
+        "scipy==1.5.2",
+    ],
     packages=["lfp", "patch_clamp"],
     entry_points={"console_scripts": ["lfp = lfp.__main__:main"]},
 )
